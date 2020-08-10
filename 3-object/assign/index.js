e927c720ed8aee95909dde59e979bf7f1b46f209
twodogbeta <qijinhaoup@163.com>
2020-08-11 07:51:23 +0800

    [Jim] finish: -object assigin

--- 3-object/assign/index.js
+++ 3-object/assign/index.js
@@ -1,3 +1,7 @@
 export default function addSerialNumber(source) {
   // TODO 5: 在这里写实现代码，需要采用ES6 Object.assign
+  const number = { serialNumber: '12345' };
+  Object.assign(source, number);
+  source.properties.status = 'processed';
+  return source;
 }
