9747664e350718319e7be14caa72eb83446989e9
twodogbeta <qijinhaoup@163.com>
2020-08-11 07:54:02 +0800

    [Jim] finish: 3-object values

--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,3 +1,9 @@
 export default function countTypesNumber(source) {
   // TODO 6: 在这里写实现代码
+  let sum = 0;
+  const valueArray = Object.values(source);
+  valueArray.forEach(value => {
+    sum += Number(value);
+  });
+  return sum;
 }
