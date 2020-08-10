c6da61dae41228783983716d2b9eaa51fcf1657b
twodogbeta <qijinhaoup@163.com>
2020-08-11 07:38:26 +0800

    [Jim] finish: 2-array fliter

--- 2-array/filter/index.js
+++ 2-array/filter/index.js
@@ -1,9 +1,11 @@
 function chooseMultiplesOfThree(collection) {
   // TODO 1: 在这里写实现代码
+  return collection.filter(n => n % 3 === 0);
 }
 
 function chooseNoRepeatNumber(collection) {
   // TODO 2: 在这里写实现代码
+  return collection.filter((word, index) => collection.indexOf(word) === index);
 }
 
 export { chooseMultiplesOfThree, chooseNoRepeatNumber };
