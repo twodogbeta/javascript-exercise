a8de8e861a10142203cc9338aa168c3bc48736fe
twodogbeta <qijinhaoup@163.com>
2020-08-11 08:06:04 +0800

    [Jim] finish: 4-class practice_4

--- 4-class/practice_4/student.js
+++ 4-class/practice_4/student.js
@@ -1,1 +1,14 @@
 // TODO 16: 在这里写实现代码
+
+import Person from './person';
+
+export default class Student extends Person {
+  constructor(name, age, klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+
+  introduce() {
+    return `${super.basic_introduce()} I am a Student. I am at Class ${this.klass}.`;
+  }
+}
--- 4-class/practice_4/worker.js
+++ 4-class/practice_4/worker.js
@@ -1,1 +1,9 @@
 // TODO 17: 在这里写实现代码
+
+import Person from './person';
+
+export default class Worker extends Person {
+  introduce() {
+    return `${super.basic_introduce()} I am a Worker. I have a job.`;
+  }
+}
