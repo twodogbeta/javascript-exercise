09b8500a227c1ecc48b7bb82694235f4a43d6f7b
twodogbeta <qijinhaoup@163.com>
2020-08-11 08:22:11 +0800

    [Jim] 5-async 1-xhr

--- 5-async/1-xhr/index.js
+++ 5-async/1-xhr/index.js
@@ -2,7 +2,16 @@ function fetchData(url, successCallback, errorCallback) {
   const xhr = new XMLHttpRequest();
   // <-- start
   // TODO 21: 通过XMLHttpRequest实现异步请求
-
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status >= 200 && xhr.status <= 299) {
+      successCallback(xhr.responseText);
+    } else if (xhr.readyState === 4 && (xhr.status < 200 || xhr.status > 299)) {
+      errorCallback(xhr.responseText);
+    }
+  };
+  xhr.open('GET', url, true);
+  xhr.responseType = 'application/json';
+  xhr.send();
   // end -->
 }
 
